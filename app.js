066ed0ff50b1d03386b53369f92329cb04db2c90
Julia Baasch <juliabaaschw@gmail.com>
2021-05-12 15:33:43 -0300

    add start script

--- app.js
+++ app.js
@@ -158,6 +158,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 })
 
-app.listen(3000, () => {
-    console.log('On 3000');
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`On ${port}`);
 })
